231ab95b49678946fe3d2de99bb9f67262d0f9d6
Dubal Antonio Aguilar Torres <dubala@uninorte.edu.co>
2025-03-20 09:05:22 -0500

    Creando la clase Cat

---
+++ src/core/Cat.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author dubala
+ */
+public class Cat {
+    private String enfermedadGatuna;
+
+    public Cat(String enfermedadGatuna) {
+        this.enfermedadGatuna = enfermedadGatuna;
+    }
+    
+}
